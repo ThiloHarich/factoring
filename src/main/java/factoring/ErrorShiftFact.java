ff3d5f5e858a3fbd57fb45a66729b53b18b0ba0f
Thilo Harich <thilo@bd4travel.com>
2016-12-23 19:45:01 +0100

    improved

--- src/main/java/factoring/ErrorShiftFact.java
+++ src/main/java/factoring/ErrorShiftFact.java
@@ -3,18 +3,19 @@ package factoring;
 public class ErrorShiftFact {
 
 	private final int maxL = 100;
+	long operations = 0;
 
 	public long findFactor(long q, long p)
 	{
 		final long n = p*q;
-		final long ySol = (p+q)/2;
+		final long ySol = (q- p)/2;
 		// factor lower factors with trial division
 
 		for (int l = 0; l < maxL ; l++) {
 			int a=1;
 			if(l>0)
 				a=7;
-			System.out.println("next a= " + a + " for new interval");
+			System.out.println("next a = " + a + " for new interval");
 			final double sqrt = Math.sqrt(n);
 			final int sqrtN = (int) Math.ceil(sqrt);
 			final double log = Math.log(n);
@@ -22,6 +23,7 @@ public class ErrorShiftFact {
 			for (int x=sqrtN; x < sqrtN + searchInterval; x++){
 				long right = x*x - n;
 				final int yFloor = (int) Math.floor(Math.sqrt(right));
+				operations++;
 
 				if (yFloor == 0)
 					return x+yFloor;
@@ -29,13 +31,15 @@ public class ErrorShiftFact {
 				for (int y=yFloor; y<yFloor+1; y++)
 				{
 					final int error = (int) Math.abs(right - y*y);
-					if ((0.0 + ySol-y)/error == Math.ceil((ySol-y)/error))
-						System.out.println("x=" + x + " right 'a' " + (ySol-y)/error);
+					final int sign = (int) Math.signum(right - y*y);
 					final long xShifted = x + a*error;
+					printSolution(ySol, a, x, y, error, sign, sqrtN, xShifted);
 					right = xShifted*xShifted - n;
 
+					operations++;
 					if (MyMath.isSquare(right))
 					{
+						printSolution(ySol, a, x, y, error, sign, sqrtN, xShifted);
 						return (long) (Math.sqrt(right) + xShifted);
 					}
 				}
@@ -43,5 +47,12 @@ public class ErrorShiftFact {
 		}
 		// no factor found
 		return -1;
+	}
+
+	protected void printSolution(final long ySol, int a, int x, int y, final int error, int sign, int sqrtN, long xShifted) {
+		if ((0.0 + ySol-y)/error == Math.ceil((ySol-y)/error))
+		{
+			System.out.println("right 'a' " + (ySol-y)/error + " \tspeedup " + (0.0 + xShifted-sqrtN)/operations + " \tsearchInterval " + (x-sqrtN) + " \tx = " + x + "\te = " + error + " \ta(2x + a|e|) - sign(e) = " + a*(2*x + error - sign));
+		}
 	}
 }
