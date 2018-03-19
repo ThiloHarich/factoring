617547b1ad877814b350e227b5bad37c19956422
Thilo Harich <thilo@bd4travel.com>
2018-03-19 17:26:38 +0100

    sdds

--- src/main/java/factoring/fermat/lehman/LehmanSingleFactorFinder.java
+++ src/main/java/factoring/fermat/lehman/LehmanSingleFactorFinder.java
@@ -1,29 +1,30 @@
 package factoring.fermat.lehman;
 
+import java.math.BigInteger;
+
 import de.tilman_neumann.math.factor.SingleFactorFinder;
 import de.tilman_neumann.types.SortedMultiset;
-
-import java.math.BigInteger;
 
 /**
  * Created by Thilo Harich on 18.03.2018.
  */
 public class LehmanSingleFactorFinder implements SingleFactorFinder {
-    @Override
-    public BigInteger findSingleFactor(BigInteger N) {
-        LehmanNoSqrtFact impl = new LehmanNoSqrtFact(41, 1.001f);
-        final long factor = impl.findPrimeFactors(N.longValue(), null);
-        return BigInteger.valueOf(factor);
-    }
+	@Override
+	public BigInteger findSingleFactor(BigInteger N) {
+		//        LehmanNoSqrtFact impl = new LehmanNoSqrtFact(41, 1.001f);
+		final LehmanReverseFact impl = new LehmanReverseFact(41, 3f);
+		final long factor = impl.findPrimeFactors(N.longValue(), null);
+		return BigInteger.valueOf(factor);
+	}
 
-    @Override
-    public SortedMultiset<BigInteger> factor(BigInteger N) {
-        // TODO
-        return null;
-    }
+	@Override
+	public SortedMultiset<BigInteger> factor(BigInteger N) {
+		// TODO
+		return null;
+	}
 
-    @Override
-    public String getName() {
-        return LehmanSingleFactorFinder.class.getSimpleName();
-    }
+	@Override
+	public String getName() {
+		return LehmanSingleFactorFinder.class.getSimpleName();
+	}
 }
